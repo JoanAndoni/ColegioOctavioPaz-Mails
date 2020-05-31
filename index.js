47e581781d5a44fc10d2ffd907081f6995b0aeee
JoanAndoni <mcr_joan@hotmail.com>
2020-05-30 23:44:16 -0500

    Mail formatted

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function authorize(credentials, callback) {
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
+        oAuth2Client.(JSON.parse(token));
         callback(oAuth2Client);
     });
 }
@@ -89,4 +89,25 @@ function listLabels(auth) {
             console.log('No labels found.');
         }
     });
+}
+
+/**
+ * Send Message.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} email RFC 5322 formatted String.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+function sendMessage(userId, email, callback) {
+    // Using the js-base64 library for encoding:
+    // https://www.npmjs.com/package/js-base64
+    var base64EncodedEmail = Base64.encodeURI(email);
+    var request = gapi.client.gmail.users.messages.send({
+        'userId': userId,
+        'resource': {
+            'raw': base64EncodedEmail
+        }
+    });
+    request.execute(callback);
 }
